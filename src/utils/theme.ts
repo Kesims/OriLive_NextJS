b46323257ba75096d0c99580ad525dfe045bb2f9
Kesims <43478239+Kesims@users.noreply.github.com>
2023-02-27 20:48:34 +0100

    re-structured login component

---
+++ src/utils/theme.ts
@@ -0,0 +1,8 @@
+import { deepOrange } from "@material-ui/core/colors";
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+	palette: {
+		primary: deepOrange,
+	},
+});
