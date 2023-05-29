9cfd83b17f6f19be74e8485ead99d4059b1d7eb6
matej <kesimsmjn1@gmail.com>
2023-05-29 10:37:44 +0200

    fix language change not being persistent

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,18 +2,22 @@ import i18next from "i18next";
 import enTranslation from "@/src/i18n/en";
 import csTranslation from "@/src/i18n/cs";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 export const initTranslations = async () => {
-    await i18next.use(initReactI18next).init({
-        resources: {
-            en: enTranslation,
-            cs: csTranslation,
-        },
-        lng: undefined,
-        fallbackLng: "en",
-        interpolation: {
-            escapeValue: true,
-        },
-        keySeparator: ".",
-    });
+    await i18next
+        .use(LanguageDetector)
+        .use(initReactI18next)
+        .init({
+            resources: {
+                en: enTranslation,
+                cs: csTranslation,
+            },
+            // lng: "en",
+            fallbackLng: "en",
+            interpolation: {
+                escapeValue: true,
+            },
+            keySeparator: ".",
+        });
 };
